ed6b1b476cc03e5e26286b56ed25d6781f533e8a
BaekSeungJoo <eunhar1@naver.com>
2023-07-24 18:00:19 +0900

    Feat : "오브젝트 클릭 시 색 변경 기능 만듦." Feat : "타워 생성 버튼 만듦."
    집에가서 추가 작업할 예정

---
+++ RandomTowerDefense/Assets/01.RanTowerDefense/Scripts/Node.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Node : MonoBehaviour
+{
+    public Color StartColor;
+    public Color SelectColor;
+    public Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        StartColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+       // rend.material.color = SelectColor;
+    }
+
+    private void OnMouseUp()
+    {
+        rend.material.color = SelectColor;
+    }
+}
---
+++ RandomTowerDefense/Assets/BuildManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public GameObject SelectNode;
+    public GameObject Tower;
+
+    public void BuildToTower()
+    {
+        Instantiate(Tower, SelectNode.transform.position, Quaternion.identity);
+    }
+}
